1f0ca4566f4cf57ebf0433bc63fdfa1099c4accf
DJ.Knight <dan90kn@Live.com.au>
2020-11-12 13:17:07 +1000

    FizBuzz now complete

--- JS Projects/Simple Tests in js/idea.js
+++ JS Projects/Simple Tests in js/idea.js
@@ -170,7 +170,9 @@ if (guestList.includes(guesName)) {
 var output = [];
 var count = 1;
 function fizzBuzz() {
-  if (count % 3 === 0) {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push("fizzBuzz");
+  } else if (count % 3 === 0) {
     output.push("Fizz");
   } else if (count % 5 === 0) {
     output.push("Buzz");
