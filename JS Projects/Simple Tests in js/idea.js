74f34813afa1107a227ae1e1b580b7e7145e76b5
DJ.Knight <dan90kn@Live.com.au>
2020-11-17 18:07:45 +1100

    fizbuz for loop

--- JS Projects/Simple Tests in js/idea.js
+++ JS Projects/Simple Tests in js/idea.js
@@ -172,9 +172,8 @@ function isLeap(year) {
 // push- adds pop- remove------------------------------
 
 var output = [];
-var count = 1;
 function fizzBuzz() {
-  while (count <= 100) {
+  for (var count = 1; count < 101; count++) {
     if (count % 3 === 0 && count % 5 === 0) {
       output.push("fizzBuzz");
     } else if (count % 3 === 0) {
@@ -184,8 +183,6 @@ function fizzBuzz() {
     } else {
       output.push(count);
     }
-    // count++ makes it add 1 2 3 etc with out its always just 1 1 1
-    count++;
   }
   console.log(output);
 }
