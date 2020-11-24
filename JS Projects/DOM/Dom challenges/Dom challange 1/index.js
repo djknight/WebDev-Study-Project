437417b872822964e29712ad63b9fadb3097eba3
DJ.Knight <dan90kn@Live.com.au>
2020-11-24 17:18:32 +1000

    Update index.js

--- JS Projects/DOM/Dom challenges/Dom challange 1/index.js
+++ JS Projects/DOM/Dom challenges/Dom challange 1/index.js
@@ -2,3 +2,4 @@ document.firstElementChild.lastElementChild.children[3].lastElementChild.innerHT
   "Dan The Man";
 
 document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
