5c234c1122b5707987fd391a96ad7a92da469e53
DJ.Knight <dan90kn@Live.com.au>
2020-12-16 16:59:47 +1000

    update

--- JS Projects/DOM/Dom challenges/Dom challange 1/index.js
+++ JS Projects/DOM/Dom challenges/Dom challange 1/index.js
@@ -19,3 +19,17 @@ var myFather = new Person("John", "Doe", 50, "blue");
 // Display age
 document.getElementById("demo").innerHTML =
   "My father is " + myFather.age + ".";
+
+///challange
+
+function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
+  this.yearsOfExperience = yearsOfExperience;
+  this.name = name;
+  this.CleaningRepertoire = cleaningRepertoire;
+  this.clean = function () {
+    alert("Cleaning in progress");
+  };
+}
+
+// type " var houseKeeper1 = new HouseKeeper (12, "james" ,["bedroom"]);
+//houseKeeper1.clean();
