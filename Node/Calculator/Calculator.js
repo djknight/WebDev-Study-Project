d07d43834798157c93d023bc0871446de3d8c186
DJ.Knight <dan90kn@Live.com.au>
2021-02-16 16:25:10 +1000

    Bmi added
    O.o

--- Node/Calculator/Calculator.js
+++ Node/Calculator/Calculator.js
@@ -7,13 +7,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-//post
+//post cal
 app.post("/", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
 
   res.send("The result is " + result);
+});
+//bmi
+app.get("/bmi", function (req, res) {
+  res.sendFile(__dirname + "/bmi.html");
+});
+app.post("/bmi", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = Math.round(weight / (height * height));
+  res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function () {
