8b6720a99c10b4a87b5d255e16282777878e188b
DJ.Knight <dan90kn@Live.com.au>
2021-03-01 16:11:59 +1000

    api stuff

---
+++ API/WeatherProject/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Brisbane, AU&appid=8147ee1c74bd05922b6f9d50ff695d23&units=metric";
+  https.get(url, function (response) {
+    //status code 200s! OK
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "<p>The Current weather in Brisbane is " + weatherDescription + "</p>"
+      );
+      res.write("<h1> The temperature is " + temp + " degrees Celsius.</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("server listening port 3000 ctrl c to close");
+  console.log("https://localhost:3000/");
+});
