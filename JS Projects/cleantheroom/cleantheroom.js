b5fdf6b08ce1fa6cb2505e91c1de1e0a387866f2
DJ.Knight <djknight90@outlook.com>
2022-03-08 12:02:27 +1000

    Create cleantheroom.js
    Code Challenge! in Cleaning

---
+++ JS Projects/cleantheroom/cleantheroom.js
@@ -0,0 +1,15 @@
+// Question 1: Clean the room function: given an input
+// of , make a function 
+// that organizes these into individual array that is ordered. 
+// For example answer(ArrayFromAbove) should return:
+// [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. 
+//  Bonus: Make it so it organizes strings differently from number types.
+// i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
+
+let arr = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+function clean (arr){
+    
+let sort =arr.sort((a,b) => a-b);
+    
+}
+for(let)
